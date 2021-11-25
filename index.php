43675c029992783abc58fe947ed77ec3f96b43fd
turovskiy <alexturik@gmail.com>
2021-11-25 13:24:52 +0200

    add readme text

--- index.php
+++ index.php
@@ -8,16 +8,22 @@
 */
 
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
+// стволюю новий php класс
 class WpReact {
   function __construct() {
     add_action('init', array($this, 'onInit'));
   }
 
   function onInit() {
+    
+    //виконую реєстрацію скриптів та стилів плагіну для адмін панелі згідно Кодексу Wordpress
+
     wp_register_script('WpReactScript', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-element', 'wp-editor'));
     wp_register_style('WpReactStyle', plugin_dir_url(__FILE__) . 'build/index.css');
     
+    //реєструю кастомний тип блоку
+    //https://developer.wordpress.org/reference/functions/register_block_type/
+
     register_block_type('wpreactnamespace/wp-react-guten-block', array(
       'render_callback' => array($this, 'renderCallback'),
       'editor_script' => 'WpReactScript',
@@ -25,8 +31,16 @@ class WpReact {
     ));
   }
 
+  //колбек функція, яка буде виконувати рендерінг конпонентів React
+
   function renderCallback($attributes) {
+
+    //перевіряю чи є поточна сторінка адмінкою
+    //якщо не адмінка => виконую реєстрацію скриптів та стилів фронта (клієнтської частини)
+
     if (!is_admin()) {
+
+      //ставлю в чергу при ініціалізації скрипти та стилі плагіну 
       wp_enqueue_script('wpReactFrontendScript', plugin_dir_url(__FILE__) . 'build/frontend.js', array('wp-element'));
       wp_enqueue_style('wpReactFrontendStyles', plugin_dir_url(__FILE__) . 'build/frontend.css');
     }
